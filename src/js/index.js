f665c8f4307f9a85baf0e3a1a5688347ace2e654
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2020-12-03 17:31:09 +0100

    water animation

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,17 @@ registerSW();
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const water = document.querySelector(".main__glass--water");
+const add = document.querySelector(".main__button--add");
+const remove = document.querySelector(".main__button--remove");
+
+add.addEventListener("click",() => {
+    water.classList.add("main__glass--add");
+    water.classList.remove("main__glass--remove");
+})
+
+remove.addEventListener("click",() => {
+    water.classList.add("main__glass--remove");
+    water.classList.remove("main__glass--add");
+})
