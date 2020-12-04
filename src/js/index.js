7e3fe4bf79013832c497e46544a5440aaa1aaef2
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2020-12-04 15:11:23 +0100

    svg changed to bg image&js counter

--- src/js/index.js
+++ src/js/index.js
@@ -8,16 +8,24 @@ registerSW();
 
 console.log('HELLO 🚀')
 
-const water = document.querySelector(".main__glass--water");
 const add = document.querySelector(".main__button--add");
 const remove = document.querySelector(".main__button--remove");
+const glassCounter = document.querySelector(".main__glass-counter--js");
+const water = document.querySelector(".main__glass--water");
+let counter = 0;
 
 add.addEventListener("click",() => {
+    counter++;
+    glassCounter.innerHTML = counter;
     water.classList.add("main__glass--add");
     water.classList.remove("main__glass--remove");
-})
+});
 
 remove.addEventListener("click",() => {
+    if(counter>0) {
+        counter--;
+        glassCounter.innerHTML = counter;
+    };
     water.classList.add("main__glass--remove");
     water.classList.remove("main__glass--add");
-})
+});
